1a17b79234a187e15ef49be2c069198773f94704
oyfkr <oyfkr@naver.com>
2021-01-12 16:09:55 +0900

    spring 싱글톤 공부 3
    스프링을 이용한 싱글톤

--- test/java/hello/core/singleton/SingletonTest.java
+++ test/java/hello/core/singleton/SingletonTest.java
@@ -3,9 +3,12 @@ package hello.core.singleton;
 import hello.core.AppConfig;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
     @Test
@@ -36,5 +39,23 @@ public class SingletonTest {
         System.out.println("singletonService2 = " + singletonService2);
 
         Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+        //AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //member1 != member2
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+
     }
 }
